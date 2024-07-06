f71f5e875be59001105ad215482e2d757ee3b37f
vieitesss <dvieitest@gmail.com>
2024-07-07 00:48:42 +0200

    format: small format of arguments and added some TODOs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,16 @@
-use std::io::{stdout, Result, Stdout};
+// ----- TODO -----
+//
+// Create "App" struct with something like:
+//     - tasks
+//     - state
+//     - ...
+// 
+// Improve project file structure:
+//     - main.rs
+//     - event.rs
+//     - ui.rs
+
+use std::io::{stdout, Result};
 
 use ratatui::{
     backend::CrosstermBackend,
@@ -7,15 +19,16 @@ use ratatui::{
         terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
         ExecutableCommand,
     },
+    prelude::Backend,
     style::Stylize,
     widgets::Paragraph,
     Terminal,
 };
 
-fn draw_ui(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<()> {
-    terminal.draw(|frame| {
-        let area = frame.size();
-        frame.render_widget(
+fn draw_ui(terminal: &mut Terminal<impl Backend>) -> Result<()> {
+    terminal.draw(|f| {
+        let area = f.size();
+        f.render_widget(
             Paragraph::new("Hello Ratatui! (press 'q' to quit)")
                 .red()
                 .on_dark_gray(),
@@ -26,7 +39,7 @@ fn draw_ui(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<()> {
     Ok(())
 }
 
-fn init_app() -> Result<Terminal<CrosstermBackend<Stdout>>> {
+fn init_app() -> Result<Terminal<impl Backend>> {
     stdout().execute(EnterAlternateScreen)?;
     enable_raw_mode()?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
@@ -48,6 +61,7 @@ fn main() -> Result<()> {
     loop {
         draw_ui(&mut terminal)?;
 
+        // 16 millis = 60 fps
         if event::poll(std::time::Duration::from_millis(16))? {
             if let event::Event::Key(key) = event::read()? {
                 if key.kind == KeyEventKind::Press {
